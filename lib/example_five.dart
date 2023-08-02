6007c749e4ff0f4aa82e2d6ad1bfa568ef3a59b9
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-03 03:57:18 +0500

    api

--- lib/example_five.dart
+++ lib/example_five.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:api_tutorials_flutter/Models/product_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -13,6 +15,12 @@ class _ExampleFiveState extends State<ExampleFive> {
 
   Future<ProductModel> getProductsApi () async {
     final response = await http.get(Uri.parse('https://webhook.site/b47feb57-0928-4ec5-8caa-87290596886c'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200){
+      return ProductModel.fromJson(data);
+    } else {
+
+    }
   }
   @override
   Widget build(BuildContext context) {
