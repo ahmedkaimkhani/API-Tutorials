6bd535a7e6391db308619d43718ed087c30718e4
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-12 04:40:26 +0500

    rest api

---
+++ lib/Rest API/Signup.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Text('Sign Up R Api'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
